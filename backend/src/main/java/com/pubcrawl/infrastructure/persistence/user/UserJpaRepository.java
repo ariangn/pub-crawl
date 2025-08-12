e3eaf581c04494a52a772fbd109c72a990c0f3af
ariangn <arianakamura@gmail.com>
2025-08-12 00:12:03 -0500

    user infra

---
+++ backend/src/main/java/com/pubcrawl/infrastructure/persistence/user/UserJpaRepository.java
@@ -0,0 +1,13 @@
+package com.pubcrawl.infrastructure.persistence.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserJpaRepository extends JpaRepository<UserJpaEntity, UUID> {
+
+    Optional<UserJpaEntity> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
